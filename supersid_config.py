26b4a0674e945cc360591ee969a18dd0e415e2cd
OharaO <oharao@tcd.ie>
2020-11-23 16:05:32 +0000

    Add README & fix typos

--- supersid_config.py
+++ supersid_config.py
@@ -9,4 +9,4 @@ Created on Mon Nov 23 09:48:10 2020
 data_path = 'C:/Users/oscar/OneDrive/Desktop/temp'
 
 # Path location where you wish the data to be stored
-archive_path = 'C:/Users/oscar/OneDrive/Desktop/SuperSidPython/data'
+archive_path = 'C:/Users/oscar/OneDrive/Desktop/SuperSid_Scripts/data'
