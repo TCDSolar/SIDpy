2f9886a0a0a065fa15689fe84ce6759cb7def9a8
OharaO <oharao@tcd.ie>
2021-02-01 14:25:25 +0000

    Update vlfclient.py

--- supersid/vlfclient.py
+++ supersid/vlfclient.py
@@ -149,13 +149,13 @@ class VLFClient:
         time, signal = data.datetime.tolist(), data.signal_strength.tolist()
 
         fig, ax1 = plt.subplots(figsize=(9, 3))
-        ax1.set(xlabel='Time: ' + time[0].strftime('%Y-%m-%d') + ' (UTC)',
+        ax1.set(xlabel='Time: (UTC)',
                 ylabel='Signal Strength (dB)',
                 title='SuperSID (' + parameters['Site'] + ', ' +
                       parameters['Country'] + ') - ' +
                       parameters['StationID'] + ' (' +
                       transmitters[parameters['StationID']][2] +
-                      ', ' + parameters['Frequency'] + 'Hz' + ')')
+                      ', ' + parameters['Frequency'][0:-3] + '.' + parameters['Frequency'][2] + 'kHz' + ')')
         ax1.xaxis.set_major_locator(dates.HourLocator(interval=2))
         ax1.xaxis.set_major_formatter(dates.DateFormatter('%H:%M'))
         ax1.plot(time, signal, color='k')
@@ -163,20 +163,10 @@ class VLFClient:
         geo = Geographic_Midpoint()
         date_time_obj = datetime.strptime(parameters['UTC_StartTime'],
                                           '%Y-%m-%d%H:%M:%S')
-        latlon = geo.calc_midpoint([float(parameters['Latitude']),
-                                    float(parameters['Longitude'])],
-                                   [transmitters[parameters['StationID']][0],
-                                    transmitters[parameters['StationID']][1]])
         try:
-            sunrise, sunset = geo.sunrise_sunset(date_time_obj.date(), latlon[0],
-                                                 latlon[1])
-            ax1.axvspan(sunrise - timedelta(minutes=10), sunrise + timedelta(minutes=10),
-                        alpha=0.25, color='orange')
-            ax1.axvspan(sunset - timedelta(minutes=10), sunset + timedelta(minutes=10),
-                        alpha=0.25, color='red')
-            #ymin, ymax = ax1.get_ylim()
-            #ax1.text(sunrise - timedelta(minutes=59), ymax - 1, 'Local Sunrise', fontsize=7, weight='bold')
-            #ax1.text(sunset - timedelta(minutes=54), ymax - 1, 'Local Sunset', fontsize=7, weight='bold')
+            sunrise, sunset = geo.sunrise_sunset(date_time_obj.date(), 53.33, 6.25)
+            ax1.axvline(sunrise, alpha=0.5, ls="dashed", color='orange', label='Dunsink Sunrise')
+            ax1.axvline(sunset, alpha=0.5, ls="dashed", color='red', label='Dunsink Sunset')
         except ValueError:
             print("Sun is always above the horizon on this day, at this location.")
         t_start = date_time_obj.replace(hour=0, minute=0, second=0, microsecond=0)
@@ -185,8 +175,9 @@ class VLFClient:
         for t in [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22]:
             ax1.axvline(t_start.replace(minute=0, second=0, microsecond=0) + timedelta(hours=t),
                         color="grey", ls="dashed", lw=0.5)
-        ax1.text(0.91, 0.03, 'Generated : ' + datetime.utcnow().strftime('%H:%M:%S'), horizontalalignment='center',
-                 verticalalignment='center', transform=ax1.transAxes, fontsize=8, weight='bold')
+        ax1.text(0.88, 0.03, 'Generated : ' + datetime.utcnow().strftime('%d-%b-%y %H:%M') + ' UTC',
+                 horizontalalignment='center', verticalalignment='center', transform=ax1.transAxes,
+                 fontsize=8)
         dpi = fig.get_dpi()
         fig.set_size_inches(1000 / float(dpi), 500 / float(dpi))
         image_path = (config_data + "/" + file_path[len(config_data) + 1:-4])
@@ -241,21 +232,10 @@ class VLFClient:
                 geo = Geographic_Midpoint()
                 date_time_obj = datetime.strptime(header['UTC_StartTime'],
                                                   '%Y-%m-%d%H:%M:%S')
-
-                latlon = geo.calc_midpoint([float(header['Latitude']),
-                                            float(header['Longitude'])],
-                                           [transmitters[header['StationID']][0],
-                                            transmitters[header['StationID']][1]])
                 try:
-                    sunrise, sunset = geo.sunrise_sunset(date_time_obj.date(), latlon[0],
-                                                         latlon[1])
-                    ax[0].axvspan(sunrise - timedelta(minutes=10), sunrise + timedelta(minutes=10),
-                                  alpha=0.25, color='orange')
-                    ax[0].axvspan(sunset - timedelta(minutes=10), sunset + timedelta(minutes=10),
-                                  alpha=0.25, color='red')
-                    #ymin, ymax = ax[0].get_ylim()
-                    #ax[0].text(sunrise - timedelta(minutes=57), ymax - 1.5, 'Local Sunrise', fontsize=6, weight='bold')
-                    #ax[0].text(sunset - timedelta(minutes=54), ymax - 1.5, 'Local Sunset', fontsize=6, weight='bold')
+                    sunrise, sunset = geo.sunrise_sunset(date_time_obj.date(), 53.33, 6.25)
+                    ax[0].axvline(sunrise, alpha=0.5, ls="dashed", color='orange', label='Dunsink Sunrise')
+                    ax[0].axvline(sunset, alpha=0.5, ls="dashed", color='red', label='Dunsink Sunset')
                 except ValueError:
                     print("Sun is always above the horizon on this day, at this location.")
 
@@ -265,12 +245,12 @@ class VLFClient:
 
                 ax[0].xaxis.set_major_locator(dates.HourLocator(interval=2))
                 ax[0].xaxis.set_major_formatter(dates.DateFormatter("%H:%M"))
-                ax[1].text(0.91, 0.03, 'Generated : ' + datetime.utcnow().strftime('%H:%M:%S'),
+                ax[1].text(0.875, 0.03, 'Generated : ' + datetime.utcnow().strftime('%d-%b-%y %H:%M') + ' UTC',
                            horizontalalignment='center', verticalalignment='center', transform=ax[1].transAxes,
-                           fontsize=8, weight='bold')
+                           fontsize=8)
 
-                ax[1].plot(gl, color='r', label='1.0-8.0 $\AA$')
-                ax[1].plot(gs, color='b', label='0.5-4.0 $\AA$')
+                ax[1].plot(gl, color='r', label='GOES 1.0-8.0 $\AA$')
+                ax[1].plot(gs, color='b', label='GOES 0.5-4.0 $\AA$')
                 ax[1].set(yscale='log', ylim=[10 ** -9, 10 ** -2])
                 ax[1].set_yticks([10 ** -8, 10 ** -7, 10 ** -6, 10 ** -5, 10 ** -4, 10 ** -3])
 
@@ -281,11 +261,9 @@ class VLFClient:
                     a.set_xlim(t_start, t_end)
                     a.tick_params(which="both", direction="in")
 
-                ax[1].legend()
-
                 ax[0].set_ylabel("Signal strength (dB)")
-                ax[1].set_ylabel("Flux Wm$^{-2}$")
-                ax[1].set_xlabel("Time: " + t_start.strftime("%m/%d/%Y") + ' (UTC)')
+                ax[1].set_ylabel("Flux (Wm$^{-2}$)")
+                ax[1].set_xlabel("Time: (UTC)")
 
                 ax[1].xaxis.set_major_locator(dates.HourLocator(interval=2))
                 ax[1].xaxis.set_major_formatter(dates.DateFormatter("%H:%M"))
@@ -293,14 +271,16 @@ class VLFClient:
                 ax2 = ax[1].twinx()
                 ax2.set(yscale='log', ylim=[10 ** -9, 10 ** -2])
                 ax2.set_yticks([3 * 10 ** -8, 3 * 10 ** -7, 3 * 10 ** -6, 3 * 10 ** -5, 3 * 10 ** -4])
-                for band in [1 * 10 ** -8, 1 * 10 ** -7, 1 * 10 ** -6, 1 * 10 ** -5, 1 * 10 ** -4]:
+                for band in [1 * 10 ** -8, 1 * 10 ** -7, 1 * 10 ** -6, 1 * 10 ** -5, 1 * 10 ** -4, 1 * 10 ** -3]:
                     ax2.axhline(band, color="grey", ls="dashed", lw=1)
                 ax2.set_yticklabels(['A', 'B', 'C', 'M', 'X'], fontsize=12)
                 ax2.tick_params(axis=u'both', which=u'both', length=0)
 
                 ax[0].set_title(
                     'SuperSID (' + header['Site'] + ', ' + header['Country'] + ') - ' + header['StationID'] + ' (' +
-                    transmitters[header['StationID']][2] + ', ' + header['Frequency'] + 'Hz' + ')')
+                    transmitters[header['StationID']][2] + ', ' + header['Frequency'][0:-3] +'.' + header['Frequency'][2] + 'kHz' + ')')
+
+                ax[1].legend(frameon=True, loc='upper center', ncol=2)
 
                 fig.tight_layout()
                 plt.subplots_adjust(hspace=0.01)
